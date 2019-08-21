905dcef3a5d39ce27a8dd3a0b42a616445594758
Gerard Eklu <gerard_eklu@homedepot.com>
2019-08-20 23:17:00 -0400

    Commented out search implementation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
-import Search from "./components/users/Search";
+// import Search from "./components/users/Search";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <div className="container">
-        <Search />
-      </div>
+      <div className="container">{/* <Search /> */}</div>
       <Users />
     </div>
   );
